6321f6132a6ab38d45e6b031d8e59157d3231075
Luckyboysiba <166699916+Luckyboysiba@users.noreply.github.com>
2026-08-08 15:26:08 +0530

    Update birthday date

--- home.js
+++ home.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // ===== CONFIGURATION - CUSTOMIZE THESE! =====
     // CUSTOMIZE: Set the birthday date (format: 'Month Day, Year HH:MM:SS')
-    const birthdayDate = new Date('October 08, 2025 00:00:00').getTime();
+    const birthdayDate = new Date('October 08, 2026 00:00:00').getTime();
 
     // CUSTOMIZE: Change this greeting message
     const greetingText = "Hey Baby! You're one of the most amazing people I've ever known! 💖";
